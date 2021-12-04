0b68d7ffee6d3b9036030699d3b1815fbeb4c2cd
HRPF <2379457118@qq.com>
2021-12-04 16:51:03 +0800

    First commit.

---
+++ app/src/main/java/com/experiment/weather/db/City.java
@@ -0,0 +1,42 @@
+package com.experiment.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCOde;
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCOde() {
+        return cityCOde;
+    }
+
+    public void setCityCOde(int cityCOde) {
+        this.cityCOde = cityCOde;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId){
+        this.provinceId = provinceId;
+    }
+}
